96c42565ceff26a7f74da50233ce1faaba64fb4c
mbnaveen <navinsmb@gmail.com>
2022-08-07 19:29:23 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       
-      <div className='mainContainer'>
+      
       
       <img src="petbackground.jpg" alt="" />
  <div className="chart-main">
